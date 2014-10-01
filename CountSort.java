9313fbf3ce91b521b8042520d16166f5182709db
huangXX <huangxinxin07@gmail.com>
2014-10-01 21:49:02 +0800

    计数排序

--- CountSort.java
+++ CountSort.java
@@ -5,23 +5,40 @@ import java.util.Arrays;
 public class CountSort {
 	public static void main (String[] args) {
 		int[] array = {2,0,1,1,1,2,2,4,4,6,1,0,1};
-		countSort(array, 6);
+		countSort_useTempArray(array, 7);
 		System.out.println(Arrays.toString(array));
 	}
 	
 	public static void countSort (int[] array, int k) {
-		int[] count = new int[k + 1];
+		int[] count = new int[k];
 		int size = array.length;
 		
 		for (int i = 0; i < size; i++) {
 			count[array[i]]++;
 		}
 		
-		for (int i = k; i >= 0; i--) {
+		for (int i = k - 1; i >= 0; i--) {
 			while (count[i]-- > 0) {
 				array[--size] = i;
 			}
 		}
+	}
+	
+	public static void countSort_useTempArray (int[] array, int k) {
+		int[] temp = new int[array.length];
+		int[] count = new int[k];
 		
+		for (int i = 0; i < array.length; i++) {
+			count[array[i]]++;
+		}
+		for (int i = 0; i < k - 1; i++) {
+			count[i + 1] += count[i];
+		}
+		for (int i = array.length - 1; i >= 0; i--) {
+			temp[--count[array[i]]] = array[i];
+		}
+		for (int i = 0; i < array.length; i++) {
+			array[i] = temp[i];
+		}
 	}
 }
